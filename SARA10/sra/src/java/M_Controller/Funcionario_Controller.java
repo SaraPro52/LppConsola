ef158114a8d7c5fc87818968fa521b1b5fe4edf4
macc22 <macastiblanco88@misena.edu.co>
2016-10-23 13:59:24 -0500

    prueba netbeans

--- SARA10/sra/src/java/M_Controller/Funcionario_Controller.java
+++ SARA10/sra/src/java/M_Controller/Funcionario_Controller.java
@@ -35,7 +35,7 @@ public class Funcionario_Controller extends HttpServlet {
                     fun[8]= new vasos().getVaso();
                     try {
                         if(new Funcionario().RegistrarFuncionario(fun)){
-                            //new CorreoController().obtenerMensaje(fun[5],fun[2],fun[8]);
+                            //new CorreoController().obtenerMensaje(fun[5],fun[2],fun[8]);PRUEBAAAA
                             respuesta.println("SI");
                         }else{
                             respuesta.println("NO");
