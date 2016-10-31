f00b543c6d90b20e616325abcb03e57b5255b386
macc22 <macastiblanco88@misena.edu.co>
2016-10-31 07:06:24 -0500

    espra

--- SARA10/sra/web/Equipo/ConsultarOa/js/consultarOa.js
+++ SARA10/sra/web/Equipo/ConsultarOa/js/consultarOa.js
@@ -1,4 +1,5 @@
 $(document).on('ready', function () {
+    alert(idRol);
     var vista = $("#vista").val();
     var objet = {opcion: 5, url: "Crud_Controller", nombre: "consutarOa", tabla: "17", datos: [""], elegir: ["4","5","6"],
         delimitador: "[{colum:2, operador:0, valor1:2}]", id: 0, opSelect: 6};
@@ -13,7 +14,7 @@ $(document).on('ready', function () {
     $("#myModal").on('show.bs.modal', function (e) {
         selector = $("#tablaListaChequeo");
         var objet = {opcion: 5, nombre: "ConsultarLista", url: "Crud_Controller", tabla: "12", datos: [""], elegir: ["0", "1", "2", "3"],
-            delimitador: "[{colum:5,operador:0 ,valor1:"+rol+"}]", id: 0, opSelect: 6};
+            delimitador: "[{colum:5,operador:0 ,valor1:"+idRol+"}]", id: 0, opSelect: 6};
         ob.setCons("Lista de chequeo");
         ob.TablaEspa(selector);
         ob.limpiarTabla(selector);
