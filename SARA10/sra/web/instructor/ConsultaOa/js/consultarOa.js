d1fa43d0615d9e92990285a28ee49b2d2f75ab6f
macc22 <macastiblanco88@misena.edu.co>
2016-11-08 23:42:25 -0500

    comite2

--- SARA10/sra/web/instructor/ConsultaOa/js/consultarOa.js
+++ SARA10/sra/web/instructor/ConsultaOa/js/consultarOa.js
@@ -34,8 +34,37 @@ $(document).on('ready', function () {
     $(document).on('change', '.clickformacion', function (e) {
         var obj = $(this);
         console.log(obj.val());
-
     });
+    //CONSULTAS
+    
+    //POR CIUDAD - SIN PARAMETROS
+    objet = {opcion: 3, url: "Crud_Controller", nombre: "ConsOaP", tabla: "6", datos: [""], elegir: ["0", "1"], id: 0, opSelect: 4};
+    
+    //POR CENTRO DE FORMACION - con parametro y sin parametro
+    objet = {opcion: 5, url: "Crud_Controller", nombre: "ConsOaP", tabla: "1", datos: [""], elegir: ["1","3"], 
+            delimitador:"[{colum:5,operador:0 ,valor1: "+IdCiudad+"}]",id: 0, opSelect: 6};
+    
+    objet = {opcion: 3, url: "Crud_Controller", nombre: "ConsOaP", tabla: "5", datos: [""], elegir: ["0", "2"], id: 0, opSelect: 4};
+    
+    //POR AREAS DE CONOCIMIENTO - con y sin paramentros
+    objet = {opcion: 5, url: "Crud_Controller", nombre: "ConsOaP", tabla: "1", datos: [""], elegir: ["7","8"], 
+            delimitador:"[{colum:5,operador:0 ,valor1: "+IdCiudad+",añadir:1},{colum:1,operador:0,valor1:"+IdCentro1+"}]",id: 0, opSelect: 6};
+    
+    objet = {opcion: 3, url: "Crud_Controller", nombre: "ConsOaP", tabla: "1", datos: [""], elegir: ["0", "1"], id: 0, opSelect: 4};
+    
+    //POR PROGRAMAS DE FORMACION - con y sin parametros
+    objet = {opcion: 5, url: "Crud_Controller", nombre: "ConsOaP", tabla: "2", datos: [""], elegir: ["4","5"], 
+            delimitador:"[{colum:1,operador:0 ,valor1: "+IdArea+"}]",id: 0, opSelect: 6};
+    
+    objet = {opcion: 3, url: "Crud_Controller", nombre: "ConsOaP", tabla: "24", datos: [""], elegir: ["0", "1"], id: 0, opSelect: 4};
+    
+    //POR TEMA - con y sin paramentros 
+    objet = {opcion: 5, url: "Crud_Controller", nombre: "ConsOaP", tabla: "3", datos: [""], elegir: ["4","5"], 
+            delimitador:"[{colum:1,operador:0 ,valor1: "+Idprograma+"}]",id: 0, opSelect: 6};
+    
+    objet = {opcion: 3, url: "Crud_Controller", nombre: "ConsOaP", tabla: "30", datos: [""], elegir: ["0", "1"], id: 0, opSelect: 4};
+    
+    
     
     $("#Formacion").hover(function(){
         selector = $("#Formacion");
