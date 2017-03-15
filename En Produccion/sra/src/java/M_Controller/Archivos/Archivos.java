fbf6067dfc8c5f4d7b97dc9de2c359df382292fb
Juan Lopez <jalopez173@misena.edu.co>
2017-03-15 01:21:12 -0500

    Cambio index

--- En Produccion/sra/src/java/M_Controller/Archivos/Archivos.java
+++ En Produccion/sra/src/java/M_Controller/Archivos/Archivos.java
@@ -22,12 +22,12 @@ public class Archivos {
     public String getBase() {
         String macc = "C:\\Users\\migue\\Desktop\\LppConsola\\En Produccion\\sra\\web\\Archivos\\ArchivosA\\";
         String juan = "C:\\Users\\JudiniBezarius\\Documents\\Proyectos\\LppConsola\\En Produccion\\sra\\web\\Archivos\\ArchivosA\\";
-        return (macc);
+        return (juan);
     }
 
     public String rutaTem() {
         String macc = "C:\\Users\\migue\\Desktop\\LppConsola\\En Produccion\\sra\\web\\Archivos\\TemArchivos\\";
         String juan = "C:\\Users\\JudiniBezarius\\Documents\\Proyectos\\LppConsola\\En Produccion\\sra\\web\\Archivos\\TemArchivos\\";
-        return (macc);
+        return (juan);
     }
 }
