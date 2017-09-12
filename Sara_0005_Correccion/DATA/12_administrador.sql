885449fba09073e5fea58b9647ddda3c66769ef9
Juan Lopez <Jalopez173@misena.edu.co>
2017-09-12 08:35:47 -0500

    sin basura

--- Sara_0005_Correccion/DATA/12_administrador.sql
+++
@@ -1,7 +0,0 @@
-use sara005;
-select * from funcionario;
-select * from rol;
-select * from rol_funcionario;
-insert into rol values(null,'administrador','mantiene el sistema a flote');
-insert into funcionario values(null,1,1019,'admi','d','sara@gmail.com','Admi','0',null,2,1);
-insert into rol_funcionario values(null,5,6);
--- Sara_0006_Correccion/APP/sra/web/coordinador/consultarReportes/js/ConsultarReportes.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function () {
-    $("#tabs").tabs();
-    $("#dataPicke1rAño").datepicker({ dateFormat: 'yy' });
-    $("#dataPicker1Mes").datepicker({ dateFormat: 'M' });
-});
